ff29d2e3ef6eee39d22f0780bd028cf354f946e9
Agataandrade <agatasouandrade@gmail.com>
2025-08-30 09:04:26 -0300

    feat navabar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import Home from './pages/home/Home'
+import Footer from "./components/footer/footer"
+import Navbar from "./components/navbar/navbar"
+import Home from "./pages/home/Home"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import './App.css'
+import Login from "./assets/login/Login"
+import Cadastro from "./cadastro/cadastro"
+
+
 
 function App() {
-    return (
-        <Home/>
-    )
+  return (
+    <>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  )
 }
 
 export default App
-
